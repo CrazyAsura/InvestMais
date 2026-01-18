33b244bc0602c720fd1afd40b50b1677f9036418
Leon <leoncdzt@gmail.com>
2026-01-18 12:37:46 -0300

    feat: setup mongoose and swagger

---
+++ backend/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      process.env.BACKEND_URL,
+      'http://localhost:8081', // Porta padrão do Expo
+      'http://localhost:19000', // Porta alternativa do Expo
+      'http://localhost:19006', // Expo Web
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
+}
+bootstrap();
